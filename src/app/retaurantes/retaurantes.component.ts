1b88d68713f5626e224a5d51e1e0cb45ba15ae47
Jose Fonseca <jm.fonsecau1@uniandes.edu.co>
2023-11-24 14:23:19 -0500

    a

---
+++ src/app/retaurantes/retaurantes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-retaurantes',
+  templateUrl: './retaurantes.component.html',
+  styleUrls: ['./retaurantes.component.css']
+})
+export class RetaurantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/retaurantes/retaurantes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RetaurantesComponent } from './retaurantes.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [RetaurantesComponent]
+})
+export class RetaurantesModule { }
